ce2033252aacfe76ceef12942f4a47782e33267f
Jklein64 <jklein.64@verizon.net>
2023-09-14 13:23:29 -0400

    Move server stuff into its own folder

--- server/server.js
+++ server/server.js
@@ -8,4 +8,4 @@ app.get("/", (request, response) => {
 
 // set up server exposed to other devices on the network
 const port = process.env.PORT ?? 8080
-app.listen(port, "10.48.153.96")
+app.listen(port)
