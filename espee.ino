f6e9f9051f2dae83c3caa4bacb2afa0e9cc38e06
jak532 <jak532@cornell.edu>
2023-09-14 13:23:29 -0400

    Move server stuff into its own folder

--- espee.ino
+++ espee.ino
@@ -45,38 +45,12 @@ void loop() {
         if (millis() - lastWebRequest > 5000) {
             Serial.println("attempting request");
             request("example.com", 80);
-//            WiFiClient client;
-//            if (client.connect("reqbin.com", 80)) {
-//              client.println("GET /echo HTTP/1.1");
-//              client.println("Host: reqbin.com");
-//              client.println("Connection: close");
-//              client.println();
-//            } else {
-//              Serial.println("connection failed");
-//            }
-//
-//            while (client.available()) {
-//              char c = client.read();
-//              Serial.print(c);
-//            }
-//
-//            if (!client.connected()) {
-//              client.stop();
-//            }
-          
-//            HttpClient http;
-//            http.begin("http://10.48.153.96:8080/");
-//            int response = http.GET();
-//            Serial.println(response);
-//            Serial.println(http.getString());
-//            http.end();
-
             lastWebRequest = millis();
         }
     }
 }
 
-void request(const char* host, uint8_t port) {
+void request(const char* host, uint16_t port) {
     Serial.print("connecting to domain: ");
     Serial.println(host);
 
@@ -87,24 +61,29 @@ void request(const char* host, uint8_t port) {
     }
 
     // This will send the request to the server
-  client.print((String)"GET / HTTP/1.1\r\n" +
-               "Host: " + String(host) + "\r\n" +
-               "Connection: close\r\n\r\n");
-  unsigned long timeout = millis();
-  while (client.available() == 0) {
-      if (millis() - timeout > 5000) {
-          Serial.println(">>> Client Timeout !");
-          client.stop();
-          return;
-      }
-  }
+    client.println("GET / HTTP/1.1");
+    client.print("Host: ");
+    client.print(host);
+    client.print(":");
+    client.println(port);
+    client.println("Connection: close");
+    client.println();
 
-  // Read all the lines of the reply from server and print them to Serial
-  while (client.available()) {
-      String line = client.readStringUntil('\r');
-      Serial.print(line);
-  }
+    unsigned long timeout = millis();
+    while (client.available() == 0) {
+        if (millis() - timeout > 5000) {
+            Serial.println("client timed out");
+            client.stop();
+            return;
+        }
+    }
 
-  Serial.println();
-  client.stop();
+    // Read all the lines of the reply from server and print them to Serial
+    while (client.available()) {
+        String line = client.readStringUntil('\r');
+        Serial.print(line);
+    }
+
+    Serial.println();
+    client.stop();
 }
