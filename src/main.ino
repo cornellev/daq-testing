fb3027cb23c04d9427f0107a0ed67b7d5fc00e8c
Jklein64 <jklein.64@verizon.net>
2023-09-29 17:21:31 -0400

    Initial code to count rising edges of RPM sensor

--- src/main.ino
+++ src/main.ino
@@ -4,13 +4,30 @@
 
 Network network;
 
+#define INTERRUPT_PIN 15
+
+volatile bool updated = false;
+volatile int count = 0;
+
+void interrupt() {
+    count++;
+    updated = true;
+}
+
 void setup() {
     Serial.begin(115200);
 
     while (!Serial)
         ;
 
+    attachInterrupt(INTERRUPT_PIN, interrupt, RISING);
     network.setup();
 }
 
-void loop() { network.loop(); }
+void loop() {
+    if (updated) {
+        Serial.println(count);
+        updated = false;
+    }
+    network.loop();
+}
